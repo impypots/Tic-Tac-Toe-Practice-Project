b949f98d0a93f3a25886de22e92ce63645d346d5
impypots <72578093+impypots@users.noreply.github.com>
2021-02-10 20:48:31 -0700

    Tictactoe Practice Page

---
+++ tictactoe/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          This is a tic tac toe Page
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
